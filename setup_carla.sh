5f4d6c16039e7853b277e8b2d1d041c5cfe7783c
SkyrimYun <36648778+SkyrimYun@users.noreply.github.com>
2024-05-05 14:58:50 +0800

    Update setup_carla.sh update carla download link

--- setup_carla.sh
+++ setup_carla.sh
@@ -3,7 +3,7 @@
 # Download and install CARLA
 mkdir carla
 cd carla
-wget https://carla-releases.s3.eu-west-3.amazonaws.com/Linux/CARLA_0.9.11.tar.gz
+wget https://carla-releases.s3.us-east-005.backblazeb2.com/Linux/CARLA_0.9.11.tar.gz
 tar -xf CARLA_0.9.11.tar.gz
 rm CARLA_0.9.11.tar.gz
 cd ..
