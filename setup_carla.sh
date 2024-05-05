f408c665a2684e7ce69ea1f3d5add8e0852f4073
SkyrimYun <36648778+SkyrimYun@users.noreply.github.com>
2024-05-05 15:19:21 +0800

    Update setup_carla.sh add carla python package install

--- setup_carla.sh
+++ setup_carla.sh
@@ -6,4 +6,16 @@ cd carla
 wget https://carla-releases.s3.us-east-005.backblazeb2.com/Linux/CARLA_0.9.11.tar.gz
 tar -xf CARLA_0.9.11.tar.gz
 rm CARLA_0.9.11.tar.gz
+
+cd PythonAPI/carla/dist/
+unzip carla-0.9.11-py3.7-linux-x86_64.egg -d carla-0.9.11-py3.7-linux-x86_64
+cd carla-0.9.11-py3.7-linux-x86_64/
+echo "install_carla"
+
+cat>setup.py<<EOF
+from distutils.core import setup
+setup(name='carla', version='0.9.11', py_modules=['carla'],)
+EOF
+
 cd ..
+pip install -e carla-0.9.11-py3.7-linux-x86_64
